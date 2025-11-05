26e9eb3ee3662ef94bf3e3e1a6d7f75ba636583c
RandomSummer <sksofiqueefiaz2015@gmail.com>
2025-11-05 11:24:14 +0530

    Fix database configuration for Render

--- Blogge/settings.py
+++ Blogge/settings.py
@@ -92,13 +92,28 @@ WSGI_APPLICATION = 'Blogge.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-    )
-}
-
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 # Password validation
